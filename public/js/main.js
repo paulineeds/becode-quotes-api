8301742520d938940e7d6de48a65d65ef3f3f5c1
Pauline De Smet <paulinedesmet@me.com>
2019-02-20 11:30:05 +0100

    first frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,35 @@
-fetch('https://hidden-journey-32557.herokuapp.com/quotes', {
-        method: "GET"
-    })
-    .then(response => response.json())
-    .then(json => getQuote(json))
-    .catch(error => alert('Error:', error));
-
-const getQuote = (data) => {
-    console.log(data);
+window.onload = () => {
+    fetch('https://hidden-journey-32557.herokuapp.com/quotes', {
+            method: "GET"
+        })
+        .then(response => response.json())
+        .then(json => getQuote(json))
+        .catch(error => alert('Error:', error));
 }
+
+
+const h3 = document.querySelector('h3');
+const p = document.querySelector('p');
+
+const handleLoad = (e) => {
+    const randomQuote = e[Math.floor(Math.random() * e.length)];
+    // console.log(randomQuote)
+    h3.textContent = `"${randomQuote.quote}"`
+    p.textContent = randomQuote.author
+}
+
+const getQuote = (quotes) => {
+    console.log(quotes);
+
+    handleLoad(quotes)
+
+    const handleClickButton = () => {
+        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        console.log(randomQuote)
+        h3.textContent = `"${randomQuote.quote}"`
+        p.textContent = randomQuote.author
+    }
+
+
+    document.querySelector("button").addEventListener('click', handleClickButton);
+};
